b4ffbeaa6595e310468f8f4323a5939bbf730ede
AditiPadha <yesaditi@gmail.com>
2018-05-30 17:22:46 +0530

    second commit

--- src/test/java/PageObjects/Loginpage.java
+++ src/test/java/PageObjects/Loginpage.java
@@ -7,7 +7,7 @@ import utility.FunctionsLibrary;
 
 public class Loginpage {
 	FunctionsLibrary fun = new FunctionsLibrary();
-	
+//adding comments	
 String username= "//input[@id='identifierId']";
 String password = "//input[@type='password']";
 String next = "//span[text()=\"Next\"]";
@@ -16,6 +16,7 @@ public void LoginGmail(WebDriver driver) {
 	driver.findElement(By.xpath(username)).sendKeys("c4aditi@gmail.com");;
 	driver.findElement(By.xpath(next)).click();
 	fun.wait(driver, By.xpath(password));
+	//fun.wait(driver, By.xpath(password));//have ti delete
 	driver.findElement(By.xpath(password)).sendKeys("aditi202016");;
 	driver.findElement(By.xpath(next)).click();
 	
